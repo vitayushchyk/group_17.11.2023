b6f18d6734f96980cb6804fa473773477f4fce79
vitayushchyk <vitay2715@gmail.com>
2023-12-01 13:53:42 +0200

    update lesson 3

--- lesson_3.py
+++ lesson_3.py
@@ -4,3 +4,8 @@ for city in cities_list:
     clear_city = ''.join([letter for letter in city if letter.isalpha()])
     clear_city = clear_city.title()
     print(f'I \u2764️ {clear_city}')
+
+# string_cities = '6..58київ\nоДеса     Львів\tжитоМИР      уЖгОрОд ХарКІВ       слАвУтИч74$:?$'
+# new_list = string_cities.replace('6..58', '').replace('74$:?$', '').title().split()
+# for city in new_list:
+#     print(f'I \u2764️ {city}')
