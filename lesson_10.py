0768070dda43a0457308e65ed60f00e0d460455f
vitayushchyk <vitay2715@gmail.com>
2023-12-19 22:29:53 +0200

    fix according feedback

--- lesson_10.py
+++ lesson_10.py
@@ -1,10 +1,23 @@
-def convert_cm_to_inches(cm):
-    return cm / 2.7
+import decimal
+
+INCHES = 2.7
+MORTGAGE_YEAR_DURATION = 25
+MORTGAGE_MONTH_DURATION = MORTGAGE_YEAR_DURATION * 12
+INCOME_THRESHOLD_PERCENT = 0.35
+
+
+def convert_cm_to_inches(cm: float) -> float:
+    result = cm / INCHES
+    return result
 
 
 def get_even_numbers(income_list: list[int]) -> list[int]:
-    return [i for i in income_list if i % 2 == 0]
+    result = [i for i in income_list if i % 2 == 0]
+    return result
 
 
-def check_mortgage(total, income) -> bool:
-    return (total / 25 / 12) < (income * 0.35)
+def check_mortgage(total: decimal, income: decimal) -> bool:
+    mortgage_per_month = decimal.Decimal(total / MORTGAGE_MONTH_DURATION)
+    income_threshold = decimal.Decimal(income * INCOME_THRESHOLD_PERCENT)
+    result = mortgage_per_month < income_threshold
+    return result
