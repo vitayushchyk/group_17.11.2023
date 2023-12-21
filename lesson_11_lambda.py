dbc92eb43312432e86b55bac0c55f49b133f5c4e
vitayushchyk <vitay2715@gmail.com>
2023-12-21 21:07:56 +0200

    lesson 11

---
+++ lesson_11_lambda.py
@@ -0,0 +1,7 @@
+names = ['Ivan', 'Vika', 66, 88, 'vadim', 'yura', 'vita']
+convert_to_str = map(lambda s: str(s), names)
+print(list(convert_to_str))
+
+numbers = [1, 4.6, 7, 8, 9, 5, 3, 'kkk', 4.6, 7.8, 9.7]
+result = list(filter(lambda x: isinstance(x, int), numbers))
+print(result)
