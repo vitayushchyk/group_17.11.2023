65a47ede3ce12886ea31eb9126a1108fe3628282
vitayushchyk <vitay2715@gmail.com>
2023-12-21 21:15:22 +0200

    lesson 11 fix

--- lesson_11_lambda.py
+++ lesson_11_lambda.py
@@ -3,5 +3,5 @@ convert_to_str = map(lambda s: str(s), names)
 print(list(convert_to_str))
 
 numbers = [1, 4.6, 7, 8, 9, 5, 3, 'kkk', 4.6, 7.8, 9.7]
-result = list(filter(lambda x: isinstance(x, int), numbers))
+result = list(filter(lambda x: type(x) == int, numbers))
 print(result)
